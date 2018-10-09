4a2ac08b4e8c6e6d2e457ed88db401fea12323fe
Mike Feller <41796396+MikeF-Code@users.noreply.github.com>
2018-10-09 11:13:18 -0400

    Input validator added

--- javascript/game.js
+++ javascript/game.js
@@ -23,7 +23,30 @@ console.log("Answer array is: " + answer);
 answerText.textContent = answer.join(' ');
 livesText.textContent = lives;
 
+function gameLogic() {
+    // For loop to check all letters in computerChoice, if keypress is correct, update "answer" array.
+    for (var j = 0; j < computerChoice.length; j++) {
+        if(computerChoice[j] === guess){
+            answer.splice(j, 1, guess);
+            console.log("Pushed guess: " + guess + " into answer array.");
+            console.log("Current answer array: " + answer);
+            answerText.textContent = answer.join(' ');
+        } else {
 
+        }
+    }
+    // Conditional statement to decrement lives and push letter into 
+    if (computerChoice.indexOf(guess, 0) === -1) {
+        lives--;
+        wrongGuesses.push(guess);
+        console.log("Wrong guess!");
+        console.log("Current wrongGuesses array: " + wrongGuesses);
+        console.log("Current lives: " + lives);
+        missedText.textContent = wrongGuesses.join(' ');
+        livesText.textContent = lives;
+    } else {
+    }
+}
     // Key listener
     document.onkeyup = function(event) {
         backgroundMusic.play();
@@ -32,30 +55,17 @@ livesText.textContent = lives;
             guess = event.key;
             console.log("User pressed key: " + guess);
 
-            // For loop to check all letters in computerChoice, if keypress is correct, update "answer" array.
-            for (var j = 0; j < computerChoice.length; j++) {
-                if(computerChoice[j] === guess){
-                    answer.splice(j, 1, guess);
-                    console.log("Pushed guess: " + guess + " into answer array.");
-                    console.log("Current answer array: " + answer);
-                    answerText.textContent = answer.join(' ');
-                } else {
-
-                }
+            // Alphabet upper case
+            if (event.keyCode >= 65 && event.keyCode <= 90) {
+                guess = guess.toLowerCase();
+                gameLogic();
+            // Alphabet lower case
+            } else if (event.keyCode >= 97 && event.keyCode <= 122) {
+                gameLogic();
+            } else {
+                alert("You didn't type a letter!  Try again!");
             }
-            // Conditional statement to decrement lives and push letter into 
-            if (computerChoice.indexOf(guess, 0) === -1) {
-                lives--;
-                wrongGuesses.push(guess);
-                console.log("Wrong guess!");
-                console.log("Current wrongGuesses array: " + wrongGuesses);
-                console.log("Current lives: " + lives);
-                missedText.textContent = wrongGuesses.join(' ');
-                livesText.textContent = lives;
 
-            }
-        } 
-         else {
-        }
+            
     }
-
+}
